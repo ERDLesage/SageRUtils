fcb9bae5968739f6c1d56ee4b8c9718ff1d73176
ERDLesage <elise.r.d.lesage@gmail.com>
2022-06-07 12:57:53 +0200

    improved GorillaMouse.FixOrder by using tidyr separate rather them strsplit. Made loop redundant :)

--- GorillaMouseTrackUtils.R
+++ GorillaMouseTrackUtils.R
@@ -15,13 +15,13 @@ library(ggplot2)
 GorillaMouse.ReadInTraces <- function(MTpath){
   oldpath <- getwd()
   setwd(MTpath)
-  print(sprintf("Reading in mouse traces from %s", MTpath))
+  cat(sprintf("Reading in mouse traces from %s\n", MTpath))
   #Create a list of the xlsx files in your working directory.
   xlsxfiles<-list.files(pattern=".xlsx")
   # fix the order
   Sorted <- GorillaMouse.FixOrder(xlsxfiles)
   #Import all the excel files in the list.
-  print(sprintf("Starting to import %d traces.",length(xlsxfiles)))
+  cat(sprintf("Starting to import %d traces.",length(xlsxfiles)))
   xlsx.df.list<-lapply(as.list(Sorted$NameList),read_excel)
   #Combine all these imported excel files into a single data frame. 
   gorilla.traj<-rbindlist(xlsx.df.list, fill=TRUE)
@@ -36,7 +36,7 @@ GorillaMouse.ReadInTraces <- function(MTpath){
 # ID and trial number
 GorillaMouse.FixOrder <- function(NameList, explain=TRUE){
   if (explain) {
-    cat("Ordering traces based on ID and Trial.\n
+    message("INFO: Ordering traces based on ID and Trial.\n
     Deriving these from the trace file (.xlsx) name, so edit code if ID and Trial 
     aren't 3rd and 8th item in the mouse trace file name.
     Returns a three column dataframe with the names of the trace files are the first,
@@ -44,16 +44,12 @@ GorillaMouse.FixOrder <- function(NameList, explain=TRUE){
     To use the sorted list, just convert the first row of the dataframe to a list, 
     e.g. as.list(var_name$NameList)")
   }
-  a<- strsplit(NameList,"-")
-  Order <- vector(mode="double", length = length(NameList))
-  Order <- tibble(IDOrder = numeric(length(NameList)), TrialOrder = numeric(length(NameList)))
-  for (i in 1:length(NameList)){
-    Order$IDOrder[i] <- as.double(a[[i]][3])
-    Order$TrialOrder[i] <- as.double(a[[i]][8])
-  }
+  Order <- NameList %>% tibble() %>% tidyr::separate(col=".", sep="-", 
+              into=c("TaskID", "Version", "ID", "i1", "i2", "i3", "TaskType", "TrialNum", "ScreenNum")) %>%
+              select(ID, TrialNum)
   Unsorted <- tibble(NameList) %>% cbind(Order)
-  Sorted_ <- Unsorted[order(Unsorted$TrialOrder), ]
-  Sorted <- Sorted_[order(Sorted_$IDOrder), ]
+  Sorted_ <- Unsorted[order(Unsorted$TrialNum), ]
+  Sorted <- Sorted_[order(Sorted_$ID), ]
   return(Sorted)
 }
 
