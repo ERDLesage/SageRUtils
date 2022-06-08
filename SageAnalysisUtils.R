50416290feb22e7fc296d0fac2d9ecb3785f129b
ERDLesage <elise.r.d.lesage@gmail.com>
2022-06-08 23:16:50 +0200

    saying it rather than outputting a list

--- SageAnalysisUtils.R
+++ SageAnalysisUtils.R
@@ -4,10 +4,11 @@
 ## PropMoreLessEqual  ####
 # gives the proportion of the second columns that is greater, equal, and smaller than the first.
 # useful as an effect size measure in Wilcoxon signed rank tests.
-function PropMoreLessEqual(FirstCol, SecondCol){
-  PropMore <- sum(ifelse(SecondCol>FirstCol, 1, 0))
-  PropEqual <- sum(ifelse(SecondCol==FirstCol, 1, 0))
-  PropLess <- sum(ifelse(SecondCol<FirstCol, 1, 0))
-  disp(sprintf("More: %0.2f | Equal: %0.2f | Less: %0.2f", PropMore, PropEqual, PropLess))
-  return(list(PropMore, PropEqual, PropLess))
+PropMoreLessEqual <- function(FirstCol, SecondCol){
+  PropMore <- sum(ifelse(SecondCol>FirstCol, 1, 0), na.rm=TRUE)/length(!is.na(ot_summary_wide_acc$Day_Block_1_3))
+  PropEqual <- sum(ifelse(SecondCol==FirstCol, 1, 0), na.rm=TRUE)/length(!is.na(ot_summary_wide_acc$Day_Block_1_3))
+  PropLess <- sum(ifelse(SecondCol<FirstCol, 1, 0), na.rm=TRUE)/length(!is.na(ot_summary_wide_acc$Day_Block_1_3))
+  output <- cbind(PropMore, PropEqual, PropLess)
+  cat(sprintf("More: %0.2f | Equal: %0.2f | Less: %0.2f", PropMore, PropEqual, PropLess))
+  return(output)
 }
